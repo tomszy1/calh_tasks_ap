2f171eb0cd75661fba84156fa08a472d7b59eae1
Tomasz Szymanski <szymanskit@interia.pl>
2022-08-06 19:46:20 +0200

    zaliczenie

--- src/generators/HFWC.py
+++ src/generators/HFWC.py
@@ -5,7 +5,32 @@ funkcji na przykład:
     - even_numbers(7) powinna zwracać kolejno: [2, 4]
     - even_numbers(15) powinna zwracać kolejno: [2, 4, 8, 10, 14]
 """
+number = 20
+
+
+# 1. sposób
+def even_numbers(n):
+    for i in range(0, n):
+        yield i + 1
+
+
+for even in even_numbers(number):
+    if even % 2 == 0:
+        if even % 3 != 0:
+            print(even)
+
+print('-_'*70)
+
+# 2. sposób
 
 
 def even_numbers(n):
+    for i in range(0, n+1):
+        if i % 2 == 0:
+            if i % 3 != 0:
+                print(i)
+        yield i
+
+
+for even in even_numbers(number):
     pass
